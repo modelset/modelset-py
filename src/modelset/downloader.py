7259473460c7c262ad8280c67d6947343dc47695
Jesús Sánchez Cuadrado <jesusc@um.es>
2022-11-11 11:30:50 +0100

    Download dataset in a temporary folder, then move to the output folder (default ~/.modelset/modelset)

--- src/modelset/downloader.py
+++ src/modelset/downloader.py
@@ -1,16 +1,19 @@
 import os
 import shutil
+import tempfile
 from argparse import ArgumentParser
 
 import wget
 
 URL_ZIP_MODELSET = 'https://github.com/modelset/modelset-dataset/releases/download/v0.9.3/modelset.zip'
-DEFAULT_DIR_MODELSET = os.path.join(os.path.expanduser('~'), '.modelset')
+DEFAULT_DIR_MODELSET = os.path.join(os.path.expanduser('~'), '.modelset', 'modelset')
 
 
 def main(args):
-    filename = wget.download(URL_ZIP_MODELSET, out=args.output_dir)
-    shutil.unpack_archive(filename, extract_dir=args.output_dir)
+    tempdir = tempfile.gettempdir()
+    filename = wget.download(URL_ZIP_MODELSET, out=tempdir)
+    shutil.unpack_archive(filename, extract_dir=tempdir)
+    shutil.move(os.path.join(tempdir, "modelset"), args.output_dir)
     os.remove(filename)
 
 
@@ -18,6 +21,6 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--output_dir",
                         help="Directory where modelset is going to be placed",
-                        default=os.path.expanduser('~'))
+                        default=DEFAULT_DIR_MODELSET)
     args = parser.parse_args()
     main(args)
