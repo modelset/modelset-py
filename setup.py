76a03d398f7e30a62181181e961c2aa84611cc0b
Jesús Sánchez Cuadrado <jesusc@um.es>
2022-12-01 22:28:22 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="modelset-py",
-    version="0.2.0",
+    version="0.2.1",
     author="Jesús Sánchez Cuadrado",
     author_email="jesusc@um.es",
     description="A libray to handle the ModelSet dataset of software models",
